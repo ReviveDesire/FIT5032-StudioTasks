182a434a637fce453e297d93b8505b0ac73e0daa
Yuhan Chen <yche0662@student.monash.edu>
2024-08-02 03:30:17 +1000

    Finish: efolio task 1,2

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1 class="green">Welcome, Yuhan Chen!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName',
+  }
+  </script>
+  
+  <style scoped>
+  p {
+    font-size: 20px;
+    color: #42b983;
+  }
+  </style>
+  
