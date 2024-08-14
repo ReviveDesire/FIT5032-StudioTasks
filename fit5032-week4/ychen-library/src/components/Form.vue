20a33bbd16092f56d1c0f70a53f56c5a89b4edf7
Yuhan Chen <yche0662@student.monash.edu>
2024-08-14 21:13:39 +1000

    Finish: efolio task 4

--- fit5032-week4/ychen-library/src/components/Form.vue
+++ fit5032-week4/ychen-library/src/components/Form.vue
@@ -7,34 +7,85 @@
                     <div class="row mb-3">
                         <div class="col-6">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" v-model="formData.username">
+                            <input type="text" class="form-control" id="username" @blur="() => validateName(true)"
+                                @input="() => validateName(false)" v-model="formData.username" />
+                            <div class="text-danger" v-if="errors.username">{{ errors.username }}</div>
                         </div>
                         <div class="col-6">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" v-model="formData.password">
+                            <input type="password" class="form-control" id="password"
+                                @blur="() => validatePassword(true)" @input="() => validatePassword(false)"
+                                v-model="formData.password" />
+                            <div class="text-danger" v-if="errors.password">{{ errors.password }}</div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-6">
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="isAustralian"
-                                    v-model="formData.isAustralian">
+                                    @blur="() => validateResident(true)" @input="() => validateResident(false)"
+                                    v-model="formData.isAustralian" />
                                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+                                <div class="text-danger" v-if="errors.resident">{{ errors.resident }}</div>
                             </div>
                         </div>
                         <div class="col-6">
                             <label for="gender" class="form-label">Gender</label>
-                            <select class="form-select" id="gender" v-model="formData.gender">
+                            <select class="form-select" id="gender" @blur="() => validateGender(true)"
+                                @input="() => validateGender(false)" v-model="formData.gender">
                                 <option value="female">Female</option>
                                 <option value="male">Male</option>
                                 <option value="other">Other</option>
                             </select>
+                            <div class="text-danger" v-if="errors.gender">{{ errors.gender }}</div>
                         </div>
                     </div>
+
                     <div class="mb-3">
-                        <label for="reason" class="form-label">Reason</label>
-                        <textarea class="form-control" id="reason" v-model="formData.reason"></textarea>
+                        <label for="reason" class="form-label">Reason for joining</label>
+                        <textarea class="form-control" id="reason" @blur="() => validateReason(true)"
+                            @input="() => validateReason(false)" v-model="formData.reason"></textarea>
+                        <div class="text-danger" v-if="errors.reason">{{ errors.reason }}</div>
                     </div>
+
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" v-model="formData.email"
+                                @blur="() => validateEmail(true)" @input="() => validateEmail(false)" />
+                            <div class="text-danger" v-if="errors.email">{{ errors.email }}</div>
+                        </div>
+                        <div class="col-6">
+                            <label for="phoneNumber" class="form-label">Phone Number</label>
+                            <input type="text" class="form-control" id="phoneNumber" v-model="formData.phoneNumber"
+                                @blur="() => validatePhoneNumber(true)" @input="() => validatePhoneNumber(false)" />
+                            <div class="text-danger" v-if="errors.phoneNumber">{{ errors.phoneNumber }}</div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <label for="dob" class="form-label">Date of Birth</label>
+                            <input type="date" class="form-control" id="dob" v-model="formData.dob"
+                                @blur="() => validateDOB(true)" @input="() => validateDOB(false)" />
+                            <div class="text-danger" v-if="errors.dob">{{ errors.dob }}</div>
+                        </div>
+                        <div class="col-6">
+                            <label for="address" class="form-label">Address</label>
+                            <input type="text" class="form-control" id="address" v-model="formData.address"
+                                @blur="() => validateAddress(true)" @input="() => validateAddress(false)" />
+                            <div class="text-danger" v-if="errors.address">{{ errors.address }}</div>
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="confirmPassword" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="confirmPassword"
+                            v-model="formData.confirmPassword" @blur="() => validateConfirmPassword(true)"
+                            @input="() => validateConfirmPassword(false)" />
+                        <div class="text-danger" v-if="errors.confirmPassword">{{ errors.confirmPassword }}</div>
+                    </div>
+
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary me-2">Submit</button>
                         <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
@@ -42,20 +93,22 @@
                 </form>
             </div>
             <div class="row mt-5" v-if="submittedCards.length">
-                <div class="d-flex flex-wrap justify-content-start">
-                    <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
-                        <div class="card-header">
-                            User Information
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Username: {{ card.username }}</li>
-                            <li class="list-group-item">Password: {{ card.password }}</li>
-                            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
-                            <li class="list-group-item">Gender: {{ card.gender }}</li>
-                            <li class="list-group-item">Reason: {{ card.reason }}</li>
-                        </ul>
-                    </div>
-                </div>
+                <DataTable :value="submittedCards" class="p-datatable">
+                    <Column field="username" header="Username"></Column>
+                    <Column field="password" header="Password"></Column>
+                    <Column field="isAustralian" header="Australian Resident">
+                        <template #body="slotProps">
+                            {{ slotProps.data.isAustralian ? 'Yes' : 'No' }}
+                        </template>
+                    </Column>
+                    <Column field="gender" header="Gender"></Column>
+                    <Column field="reason" header="Reason"></Column>
+                    <Column field="email" header="Email"></Column>
+                    <Column field="dob" header="Date of Birth"></Column>
+                    <Column field="address" header="Address"></Column>
+                    <Column field="phoneNumber" header="Phone Number"></Column>
+                    <Column field="confirmPassword" header="Confirmed Password"></Column>
+                </DataTable>
             </div>
         </div>
     </div>
@@ -63,21 +116,47 @@
 
 <script setup>
 import { ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
 
 const formData = ref({
     username: '',
     password: '',
     isAustralian: false,
     reason: '',
-    gender: ''
+    gender: '',
+    confirmPassword: '',
+    email: '',
+    dob: '',
+    address: '',
+    phoneNumber: ''
 });
 
 const submittedCards = ref([]);
 
 const submitForm = () => {
-    submittedCards.value.push({
-        ...formData.value
-    });
+    validateName(true);
+    validatePassword(true);
+    validateResident(true);
+    validateGender(true);
+    validateReason(true);
+    validateConfirmPassword(true);
+    validateEmail(true);
+    validateDOB(true);
+    validateAddress(true);
+    validatePhoneNumber(true);
+
+    if (!errors.value.username && !errors.value.password
+        && !errors.value.resident && !errors.value.gender
+        && !errors.value.reason && !errors.value.confirmPassword
+        && !errors.value.email && !errors.value.dob
+        && !errors.value.address && !errors.value.phoneNumber) {
+        submittedCards.value.push({
+            ...formData.value
+        });
+        clearForm();
+    }
 };
 
 const clearForm = () => {
@@ -86,10 +165,131 @@ const clearForm = () => {
         password: '',
         isAustralian: false,
         reason: '',
-        gender: ''
+        gender: '',
+        confirmPassword: '',
+        email: '',
+        dob: '',
+        address: '',
+        phoneNumber: ''
     };
-    submittedCards.value = [];
 };
+
+const errors = ref({
+    username: null,
+    password: null,
+    resident: null,
+    gender: null,
+    reason: null,
+    confirmPassword: null,
+    email: null,
+    dob: null,
+    address: null,
+    phoneNumber: null
+});
+
+const validateName = (blur) => {
+    if (formData.value.username.length < 3) {
+        if (blur) errors.value.username = "Name must be at least 3 characters";
+    } else {
+        errors.value.username = null;
+    }
+};
+
+const validatePassword = (blur) => {
+    const password = formData.value.password;
+    const minLength = 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (password.length < minLength) {
+        if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`;
+    } else if (!hasUppercase) {
+        if (blur) errors.value.password = "Password must contain at least one uppercase letter.";
+    } else if (!hasLowercase) {
+        if (blur) errors.value.password = "Password must contain at least one lowercase letter.";
+    } else if (!hasNumber) {
+        if (blur) errors.value.password = "Password must contain at least one number.";
+    } else if (!hasSpecialChar) {
+        if (blur) errors.value.password = "Password must contain at least one special character.";
+    } else {
+        errors.value.password = null;
+    }
+};
+
+const validateResident = (blur) => {
+    if (!formData.value.isAustralian) {
+        if (blur) errors.value.resident = "You must check the box to confirm that you are an Australian resident."
+    } else {
+        errors.value.resident = null;
+    }
+};
+
+const validateGender = (blur) => {
+    if (!formData.value.gender) {
+        if (blur) errors.value.gender = "You must select one gender."
+    } else {
+        errors.value.gender = null;
+    }
+};
+
+const validateReason = (blur) => {
+    const minLength = 3;
+    if (formData.value.reason.length < minLength) {
+        if (blur) errors.value.reason = `Reason must be at least ${minLength} characters long.`;
+    } else {
+        errors.value.reason = null;
+    }
+};
+
+// efolio task 4.2 HD
+
+const validateEmail = (blur) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(formData.value.email)) {
+        if (blur) errors.value.email = "Please enter a valid email address.";
+    } else {
+        errors.value.email = null;
+    }
+};
+
+const validatePhoneNumber = (blur) => {
+    const phoneRegex = /^\d{10}$/; // Assuming a 10-digit phone number
+    if (!phoneRegex.test(formData.value.phoneNumber)) {
+        if (blur) errors.value.phoneNumber = "Please enter a valid 10-digit phone number.";
+    } else {
+        errors.value.phoneNumber = null;
+    }
+};
+
+const validateDOB = (blur) => {
+    const today = new Date();
+    const dob = new Date(formData.value.dob);
+    const age = today.getFullYear() - dob.getFullYear();
+    if (age < 18) {
+        if (blur) errors.value.dob = "You must be at least 18 years old.";
+    } else {
+        errors.value.dob = null;
+    }
+};
+
+const validateConfirmPassword = (blur) => {
+    if (formData.value.password !== formData.value.confirmPassword) {
+        if (blur) errors.value.confirmPassword = "Passwords do not match.";
+    } else {
+        errors.value.confirmPassword = null;
+    }
+};
+
+const validateAddress = (blur) => {
+    if (formData.value.address.trim() === "") {
+        if (blur) errors.value.address = "Address cannot be empty.";
+    } else {
+        errors.value.address = null;
+    }
+};
+
 </script>
 
 <style scoped>
--- fit5032-week4/ychen-library/src/main.js
+++ fit5032-week4/ychen-library/src/main.js
@@ -7,4 +7,20 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import './style.css'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'light',
+            cssLayer: false
+        }
+    }
+});
+
+
 createApp(App).mount('#app')
