7f229015b61672ddfc1438094b9f60d061bc15c2
Yuhan Chen <yche0662@student.monash.edu>
2024-10-14 00:17:14 +1100

    Trigger deployment

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="header">
-            <h1>WEATHER APP</h1>
+            <h1>WEATHER APP TEST for week12</h1>
             <div class="search-bar">
                 <input type="text" v-model="city" placeholder="Enter city name" class="search-input" />
                 <button @click="searchByCity" class="search-button">Search</button>
